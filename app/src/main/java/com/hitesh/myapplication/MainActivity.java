a9c6d078850f319eeaa4fe751c3435e1bd62c6b3
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-08 07:55:24 +0530

    Add start  time and end time text fields
    Signed-off-by: hiteshhv <46933160+hiteshhv@users.noreply.github.com>

--- app/src/main/java/com/hitesh/myapplication/MainActivity.java
+++ app/src/main/java/com/hitesh/myapplication/MainActivity.java
@@ -2,26 +2,36 @@ package com.hitesh.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     Button play;
     MediaPlayer mediaPlayer;
     SeekBar seekBar;
+    TextView startTimeField;
+    TextView endTimeField;
 
     double startTime = 0;
     double finalTime;
     int currentPosition;
     Handler handler = new Handler();
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startTimeField = findViewById(R.id.startTimeField);
+        endTimeField = findViewById(R.id.endTimeField);
 
         play = (Button) findViewById(R.id.button);
         play.setText(">");
@@ -31,7 +41,13 @@ public class MainActivity extends AppCompatActivity {
         finalTime = mediaPlayer.getDuration();
         startTime = mediaPlayer.getCurrentPosition();
         seekBar.setProgress((int)startTime);
+        startTimeField.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) startTime)));
+        endTimeField.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                TimeUnit.MILLISECONDS.toSeconds((long) finalTime)));
+
     }
+    @SuppressLint("DefaultLocale")
     public void play(View v) {
         if (mediaPlayer.isPlaying()){
             mediaPlayer.pause();
@@ -52,9 +68,14 @@ public class MainActivity extends AppCompatActivity {
 
     //define runnable param for input
     private Runnable updateSeekBarValue = new Runnable() {
+        @SuppressLint("DefaultLocale")
         public void run() {
             startTime = mediaPlayer.getCurrentPosition();
             seekBar.setProgress((int)startTime);
+            startTimeField.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) startTime)));
+            endTimeField.setText(String.format("%d:%d", TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                    TimeUnit.MILLISECONDS.toSeconds((long) finalTime)));
             handler.postDelayed(this, 50);
         }
     };
